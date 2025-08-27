d7ce136e52f75b7eeb3ed6acf14de1e124fab359
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2025-08-26 17:29:38 -0700

    update 8

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
 os.system("conda install yaml")
 os.system("pip install -r requirements.txt")
-# os.system("pip install --upgrade tensorflow")
-# os.system("pip install --upgrade scipy")
+os.system("pip install --upgrade tensorflow")
+os.system("pip install --upgrade scipy")
